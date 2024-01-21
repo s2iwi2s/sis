0f798d07eb39e12a0941f6f91be3e641a827df5f
s2iwi2s <s2iwi2s@gmail.com>
2024-01-20 21:55:53 -0800

    add paperclip icon for add attachment

--- src/main/webapp/app/config/font-awesome-icons.ts
+++ src/main/webapp/app/config/font-awesome-icons.ts
@@ -38,6 +38,7 @@ import {
   faUsersCog,
   faWrench,
   faClipboard,
+  faPaperclip,
   // jhipster-needle-add-icon-import
 } from '@fortawesome/free-solid-svg-icons';
 
@@ -81,5 +82,6 @@ export const fontAwesomeIcons = [
   faUsersCog,
   faWrench,
   faClipboard,
+  faPaperclip,
   // jhipster-needle-add-icon-import
 ];
