bf4ef54916bd3cca34a519c311a1d34a157dda4f
Winston Pidor <48598006+s2iwi2s@users.noreply.github.com>
2026-07-21 21:19:17 -0700

    fix seq start indexes

---
+++ src/main/webapp/app/shared/json/json-pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'json',
+})
+export class JsonPipe implements PipeTransform {
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return JSON.stringify(value);
+  }
+}
